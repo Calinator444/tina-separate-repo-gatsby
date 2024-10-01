8c220ed1f1246e4b3b85f22b94d71c2e36e73660
Calinator444 <caleb.williams5247@gmail.com>
2024-10-01 12:21:19 +1000

    populated tina mardown component usign gatsby-node at build time

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,17 +7,29 @@
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
+
+const mdx = `### Hello world!
+This is the first paragraph. It contains some text.
+
+This is the second paragraph. It is separated from the first paragraph by a blank line.
+
+This is the third paragraph.
+`
+const { parseMDX } = require("@tinacms/mdx")
+
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
+    context: {
+      mdx: parseMDX(mdx, { field: { parser: { type: "markdown" } } }),
+    },
     defer: true,
   })
 }
 
-const express = require("express");
+const express = require("express")
 exports.onCreateDevServer = ({ app }) => {
-  app.use("/admin", express.static("public/admin"));
-};
+  app.use("/admin", express.static("public/admin"))
+}
--- src/templates/using-dsg.js
+++ src/templates/using-dsg.js
@@ -1,25 +1,20 @@
-import * as React from "react"
-import { Link } from "gatsby"
-
+import React from "react"
+import { TinaMarkdown } from "tinacms/dist/rich-text"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const UsingDSG = () => (
-  <Layout>
-    <h1>
-      Hello from a <b>DSG Page</b>
-    </h1>
-    <p>This page is not created until requested by a user.</p>
-    <p>
-      To learn more, head over to our{" "}
-      <a href="https://www.gatsbyjs.com/docs/reference/rendering-options/deferred-static-generation/">
-        documentation about Deferred Static Generation
-      </a>
-      .
-    </p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const UsingDSG = ({ pageContext }) => {
+  const { text, mdx } = pageContext
+
+  return (
+    <Layout>
+      <h1>Tina markdown test</h1>
+      <p>The content below is rendering using the TinaMarkdown component:</p>
+
+      <TinaMarkdown content={mdx} />
+    </Layout>
+  )
+}
 
 export const Head = () => <Seo title="Using DSG" />
 
